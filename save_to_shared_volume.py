95db7018670de0d5fa87993aa0b57e9543f9ed8d
Luis <luisaguirre027@gmail.com>
2021-10-27 19:50:01 -0400

    Adjust file paths

--- save_to_shared_volume.py
+++ save_to_shared_volume.py
@@ -1,20 +1,29 @@
-def df__save_to_shared_volume(df, file_name: str, workspaceName: str = "default", dataset_type: str = "scoring"):
+def df__save_to_shared_volume(df, file_name: str, workspace_name: str = "default", dataset_type: str = "scoring"):
 
-    dataset_types = {
+    directories = {
         "scoring": "prediction",
         "training": "training"
     }
 
-    dataset_type = dataset_types[dataset_type]
+    directory = directories[dataset_type]
+
 
     if not len(file_name):
-        import uuid
-        file_name = f"dataset-{str(uuid.uuid4())}.csv"
+        file_name = workspace_name
+        if dataset_type == "training":
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file_name = f"{file_name}-{now}"
 
     if not file_name.endswith(".csv"):
         file_name = f"{file_name}.csv"
 
-    path = f"dataroot/projects/{workspaceName}/datasets/{dataset_type}/{file_name}"
+
+    if dataset_type == "scoring":
+        now = datetime.datetime.now().strftime("%Y-%m-%d")
+        directory = f"{directory}/{now}"
+
+    path = f"dataroot/projects/{workspace_name}/datasets/{directory}/{file_name}"
+
 
     from optimus.helpers.functions import path_is_local, prepare_path_local
     if path_is_local(path):
