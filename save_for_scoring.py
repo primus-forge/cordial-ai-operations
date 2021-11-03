55da04d42e36ccbff61b64fda4752c2b8f10c846
Luis <luisaguirre027@gmail.com>
2021-11-03 17:36:36 -0400

    Adjust operations

---
+++ save_for_scoring.py
@@ -0,0 +1,20 @@
+def df__save_for_scoring(df, file_name: str, workspace_name: str = "default"):
+
+    if not len(file_name):
+        file_name = df.meta.get("file_name", workspace_name)
+
+    if not file_name.endswith(".csv"):
+        file_name = f"{file_name}.csv"
+
+    from datetime import datetime
+    now = datetime.now().strftime("%Y-%m-%d")
+
+    path = f"/dataroot/projects/{workspace_name}/datasets/prediction/{now}/{file_name}"
+
+
+    from optimus.helpers.functions import path_is_local, prepare_path_local
+    if path_is_local(path):
+        prepare_path_local(path)
+
+    df.save.csv(path)
+    return df
---
+++ save_for_training.py
@@ -0,0 +1,20 @@
+def df__save_for_training(df, file_name: str, workspace_name: str = "default"):
+
+    if not len(file_name):
+        file_name = df.meta.get("file_name", workspace_name)
+
+    # from datetime import datetime
+    # now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # file_name = f"{file_name}-{now}"
+
+    if not file_name.endswith(".csv"):
+        file_name = f"{file_name}.csv"
+
+    path = f"/dataroot/projects/{workspace_name}/datasets/training/{file_name}"
+
+    from optimus.helpers.functions import path_is_local, prepare_path_local
+    if path_is_local(path):
+        prepare_path_local(path)
+
+    df.save.csv(path)
+    return df
